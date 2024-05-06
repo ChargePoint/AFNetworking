b61f3e026727f99f291cd8f4ddf8d74d58797047
Rishab Sukumar <rishab.sukumar@chargepoint.com>
2024-05-06 14:46:58 -0700

    Add privacy manifest

--- Package.swift
+++ Package.swift
@@ -34,4 +34,5 @@ let package = Package(name: "AFNetworking",
                                           targets: ["AFNetworking"])],
                       targets: [.target(name: "AFNetworking",
                                         path: "AFNetworking",
+                                        resources: [.copy("AFNetworking/PrivacyInfo.xcprivacy")]
                                         publicHeadersPath: "")])
